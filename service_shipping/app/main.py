2350a8e2ce8876517e29aa71b01233573755796c
mhyubr <mhyubr@gmail.com>
2023-06-07 23:48:45 +0700

    add services shipping

--- service_shipping/app/main.py
+++ service_shipping/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
-from app.rajaongkir import get_shipping_cost
+from app.rajaongkir import get_shipping_cost, get_shipping_city
 
 app = FastAPI()
 
-@app.post("/shipping")
+@app.post("/shipping/cost")
 def get_shipping(api_key: str, origin: str, destination: str, weight: int, courier: str):
     shipping_cost = get_shipping_cost(api_key, origin, destination, weight, courier)
     return {"shipping_cost": shipping_cost}
+
+@app.get("/shipping/cities")
+def get_shipping_cities(api_key: str, id: str, province: str):
+    shipping_cities = get_shipping_city(api_key, id, province)
+    return {"shipping_cities": shipping_cities}
--- service_shipping/app/rajaongkir.py
+++ service_shipping/app/rajaongkir.py
@@ -2,23 +2,49 @@ import requests
 
 def get_shipping_cost(api_key: str, origin: str, destination: str, weight: int, courier: str):
     url = "https://api.rajaongkir.com/starter/cost"
-    payload = {
-        "origin": origin,
-        "destination": destination,
-        "weight": weight,
-        "courier": courier
-    }
+    # payload = {
+    #     "Origin": origin,
+    #     "Destination": destination,
+    #     "Weight": weight,
+    #     "Courier": courier
+    # }
+    payload = f"origin={origin}&destination={destination}&weight={weight}&courier={courier}"
     headers = {
-        "content-type": "application/json",
+        "content-type": "application/x-www-form-urlencoded ",
         "key": api_key,
     }
     response = requests.post(url, data=payload, headers=headers)
-    print(response)
     response_data = response.json()
-    # shipping_cost = None
-    # if response_data["rajaongkir"]["status"]["code"] == 200:
-    #     results = response_data["rajaongkir"]["results"]
-    #     if len(results) > 0:
-    #         shipping_cost = results[0]["costs"][0]["cost"][0]["value"]
-    # return shipping_cost
-    return response_data
+    shipping_cost = None
+    if response_data["rajaongkir"]["status"]["code"] == 200:
+        results = response_data["rajaongkir"]["results"]
+        if len(results) > 0:
+            shipping_cost = results[0]["costs"][0]["cost"][0]["value"]
+    elif response_data["rajaongkir"]["status"]["code"] == 400:
+        return response_data["rajaongkir"]["status"]["description"]
+    return shipping_cost
+    # return {"Origin": origin, "Destination": destination, "Weight": weight, "Courier": courier}
+
+def get_shipping_city(api_key: str, id: str, province: str):
+    url = "	https://api.rajaongkir.com/starter/city"
+
+    payload = f"id={id}&province={province}"
+    headers = {
+        "content-type": "application/x-www-form-urlencoded ",
+        "key": api_key,
+    }
+
+    response = requests.get(url, params=payload, headers=headers)
+    response_data = response.json()
+    shipping_city = None
+
+    if response_data["rajaongkir"]["status"]["code"] == 200:
+        results = response_data["rajaongkir"]["results"]
+        if len(results) > 0:
+            shipping_city = [city["city_name"] for city in results]
+    elif response_data["rajaongkir"]["status"]["code"] == 400:
+        return response_data["rajaongkir"]["status"]["description"]
+
+    return shipping_city
+
+    # return response_data["rajaongkir"]["results"]
